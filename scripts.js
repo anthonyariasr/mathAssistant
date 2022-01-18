e3efa54c916126e07dc332f6103a16469b453614
Anthony Arias <anthonyariasr@outlook.com>
2022-01-18 14:50:10 -0600

    The geometry section is finished, and the web already clean the canvas where all the other sections will be displayed

--- scripts.js
+++ scripts.js
@@ -1,22 +1,54 @@
 
-function writethings(){
-    document.getElementsByClassName("arrow")[0].style.display = "block";
-    document.getElementsByClassName("content")[0].style.display = "block";
-    const canvas = document.getElementsByClassName("canvas")[0];
-    canvas.style.display = "flex"   
+function Clean(){
+    const canvas = document.getElementsByClassName("canvas");
+    for (var i = 0; i < canvas.length; i++) {
+        canvas[i].style.display = 'none';        
+    }
 }
+
+function InitDisplay(){
+    Clean()
+    document.getElementsByClassName("arrow")[0].style.display = "block";
+    document.getElementsByClassName("content")[0].style.display = "block";   
+}
+
+function DisplayGeometry(){
+    InitDisplay()
+    const canvas = document.getElementsByClassName("cv_geometry")[0];
+    canvas.style.display = "flex"
+}
+
+function DisplayDiscounts(){
+    InitDisplay()
+    const canvas = document.getElementsByClassName("cv_discounts")[0];
+    canvas.style.display = "flex"
+}
+
+function DisplayStatistics(){
+    InitDisplay()
+    const canvas = document.getElementsByClassName("cv_statistics")[0];
+    canvas.style.display = "flex"
+}
+
+function DisplaySalaries(){
+    InitDisplay()
+    const canvas = document.getElementsByClassName("cv_salaries")[0];
+    canvas.style.display = "flex"
+}
+
+//Geometry section functions
 
 //Square functions
 
 function SquarePerimeter(){
     const side = document.getElementById("squareInput").value;
-    perimeter = side * 4;
+    const perimeter = side * 4;
     document.getElementById("squareAnswer").value = perimeter + "cm";
 }
 
 function SquareArea(){
     const side = document.getElementById("squareInput").value;
-    area = side * side;
+    const area = side * side;
     document.getElementById("squareAnswer").value = area + "cm^2";
 }
 
@@ -24,14 +56,27 @@ function SquareArea(){
 
 function TriPerimeter(){
     const side = document.getElementById("triInput").value;
-    perimeter = side * 3;
+    const perimeter = side * 3;
     document.getElementById("triAnswer").value = perimeter + "cm";
 }
 
 function TriArea(){
     const side = document.getElementById("triInput").value;
-    Area = (Math.sqrt(3)/4) * (side**2)
-    document.getElementById("triAnswer").value = Area + "cm^2";
+    const area = (Math.sqrt(3)/4) * (side**2)
+    document.getElementById("triAnswer").value = area + "cm^2";
 }
 
+//Circle functions
+
+function CirclePerimeter(){
+    const radio = document.getElementById("circleInput").value;
+    const perimeter = (radio * 2) * Math.PI;
+    document.getElementById("circleAnswer").value = perimeter + "cm";
+}
+
+function CircleArea(){
+    const radio = document.getElementById("circleInput").value;
+    const area = Math.PI * (radio ** 2);
+    document.getElementById("circleAnswer").value = area + "cm^2";
+}
 
